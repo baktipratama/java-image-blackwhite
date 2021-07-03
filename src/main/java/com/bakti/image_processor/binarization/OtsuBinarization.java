c61874e30b169318a95bfcfbbddc01d1bb232644
bakti.yudha <bakti.yudha@go-jek.com>
2021-07-04 07:58:19 +0800

    fix otsuBinarization to use Color object

--- src/main/java/com/bakti/image_processor/binarization/OtsuBinarization.java
+++ src/main/java/com/bakti/image_processor/binarization/OtsuBinarization.java
@@ -3,6 +3,7 @@ package com.bakti.image_processor.binarization;
 import com.bakti.image_processor.ImageConverter;
 import com.bakti.image_processor.grayscale.Grayscale;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class OtsuBinarization implements ImageConverter {
@@ -16,11 +17,9 @@ public class OtsuBinarization implements ImageConverter {
         int histogram[] = new int[256];
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
-                // get one pixel color
-                int pixel = grayscaledImage.getRGB(x, y);
-
                 // retrieve color of all channels
-                int R = (0x00ff0000 & pixel) >> 16;
+                Color c = new Color(image.getRGB(x, y));
+                int R = c.getRed();
                 histogram[R]++;
             }
 
@@ -64,16 +63,14 @@ public class OtsuBinarization implements ImageConverter {
                 // get one pixel color
                 int pixel = grayscaledImage.getRGB(x, y);
                 // retrieve color of all channels
-                int R = (0x00ff0000 & pixel) >> 16;
+                Color c = new Color(image.getRGB(x, y));
+                int R = c.getRed();
                 // take conversion up to one single value
-
-                if (R < treshold)
-                    R = 0;
-                else
-                    R = 255;
-
+                int a1=0;
+                if (R >= treshold) a1 = 255;
                 // set new pixel color to output bitmap
-                convertedImage.setRGB(x, y, R);
+                Color BWColor = new Color(a1, a1, a1);
+                convertedImage.setRGB(x, y, BWColor.getRGB());
             }
         }
         return convertedImage;
